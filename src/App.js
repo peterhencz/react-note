34f4d94bc34232e363b82bfba166c6bc152b8f3e
Peter Hencz <peter.hencz@gmail.com>
2018-11-05 22:57:28 +0100

    ffdfd

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            cica
+            kiscica
           </a>
         </header>
       </div>
