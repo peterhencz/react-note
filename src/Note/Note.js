5f1ad3ad15fd01a7c4c13b073c1a9a99a94678bb
Peter Hencz <peter.hencz@gmail.com>
2018-11-05 23:09:59 +0100

    create app

---
+++ src/Note/Note.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Note.css";
+import PropTypes from "prop-types";
+
+class Note extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(props) {
+    return <div>Hello</div>;
+  }
+}
+
+Note.PropTypes = {};
+export default Note;
